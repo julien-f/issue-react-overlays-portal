40c2442549c4c45c5d3ce91bfc0c61eaecc2c59b
wescoeur <ronan.abhamon@gmail.com>
2016-06-08 09:31:40 +0200

    Portal example with success and failure.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Portal } from 'react-overlays'
 import { render } from 'react-dom'
 
-const MyComponent = () => <h1>Hellow world!</h1>
+const CONTAINER_STYLE = { backgroundColor: 'green' }
+
+class PortalExample extends React.Component {
+  render () {
+    return (
+      <div>
+        {/* container is defined and it is displayed
+            with a green bg. */}
+        <p>Success:</p>
+        <div ref='container' style={CONTAINER_STYLE} />
+        <Portal container={() => this.refs.container}>
+          <p>Hello!</p>
+        </Portal>
+
+        {/* Here, container2 ref is undefined and it
+            is displayed in body. */}
+        <p>Fail:</p>
+        <Portal container={() => this.refs.container2}>
+          <p>Hello!</p>
+        </Portal>
+        <div ref='container2' style={CONTAINER_STYLE} />
+      </div>
+    )
+  }
+}
 
 render(
-  <MyComponent />,
+  <PortalExample />,
   document.getElementById('app')
 )
